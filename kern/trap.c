2a1e6704434cf19748357825a90a4c093b7a5c17
Daniel Feduk <daniel.feduk.mail@gmail.com>
2026-06-24 14:21:54 +0200

    kill proc on trap
    if running process when unexpected trap, we now kill it instead of
    panicing

--- kern/trap.c
+++ kern/trap.c
@@ -47,8 +47,16 @@ trap(struct trapframe *tf)
 		lapiceoi();
 		break;
 	default:
-		cprintf("unexpected trap %d from cpu %d rip %p (cr2=0x%p)\n", tf->trapno, cpuid(), tf->rip, rcr2());
-		panic("trap");
+		if(myproc() == 0 || (tf->cs & 3) == 0) {
+			cprintf("unexpected trap %d err %d from cpu %d rip %p (cr2=0x%p)\n", tf->trapno, tf->errcode, cpuid(), tf->rip, rcr2());
+			panic("trap");
+		} else {
+			cprintf("pid %d %s: trap %d err %d on cpu %d "
+				"rip 0x%x addr 0x%x--kill proc\n",
+				myproc()->pid, myproc()->name, tf->trapno,
+				tf->errcode, cpuid(), tf->rip, rcr2());
+			myproc()->killed = 1;
+		}
 	}
 
 	// if process killed and in userspace, exit.
